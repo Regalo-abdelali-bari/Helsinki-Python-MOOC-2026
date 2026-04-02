2896221b6c8e6551542517b05850a701839dc927
Abdelali <bariabdelali510@gmail.com>
2026-04-02 13:31:21 +0100

    Part 6 - Fruit market

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,13 @@
 # write your solution here
+def read_fruits():
+    with open("fruits.csv") as fruits:
+        d = {}
+        for fruit in fruits:
+            fruit = fruit.replace(";"," ")
+            fruit = fruit.split()
+            d[fruit[0]]=float(fruit[1])
+            
+    return d
+            
+if __name__ == "__main__":
+    print(read_fruits())
