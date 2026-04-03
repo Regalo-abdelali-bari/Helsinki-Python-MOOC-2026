376a7de784f52250747c2094dea406e10957eef6
Abdelali <bariabdelali510@gmail.com>
2026-04-03 09:31:47 +0100

    Add all previous exercises from Part 1 to 6

---
+++ part06-03_matrix/src/README.md
@@ -0,0 +1,2 @@
+# Helsinki-Python-MOOC-2026
+This is a course Python. 
--- part06-03_matrix/src/matrix.py
+++ part06-03_matrix/src/matrix.py
@@ -1,1 +1,39 @@
 # write your solution here
+
+def matrix_sum():
+    with open("matrix.txt") as matrix:
+        i = 0
+        for items in matrix:
+            items = items.replace(","," ").split()
+            for item in items:
+                item = int(item)
+                i += item
+        return i
+
+def matrix_max():
+    with open("matrix.txt") as matrix:
+        i = 0
+        for items in matrix:
+            items = items.replace(","," ").split()
+            for item in items:
+                item = int(item)
+                if item > i:
+                    i = item
+        return i
+    
+def row_sums():
+    with open("matrix.txt") as matrix:
+        i = 0
+        my_list= []
+        for items in matrix:
+            items = items.replace(","," ").split()
+            for item in items:
+                item = int(item)
+                i += item
+            my_list.append(i)
+            i = 0   
+        return my_list
+if __name__ == "__main__":
+    print(matrix_sum())
+    print(matrix_max())
+    print(row_sums())
