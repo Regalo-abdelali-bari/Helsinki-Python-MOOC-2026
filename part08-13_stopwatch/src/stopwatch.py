a4965708df17ab7f1ae1aaf1c7d9e182fbc0f796
Abdelali <bariabdelali510@gmail.com>
2026-05-06 07:12:01 +0100

    Part8 - Stopwatch

---
+++ part08-13_stopwatch/src/stopwatch.py
@@ -0,0 +1,36 @@
+# Write your solution here:
+class Stopwatch:
+    def __init__(self):
+        self.seconds = 0
+        self.minutes = 0
+
+    def tick(self):
+        self.seconds += 1
+        if self.seconds == 60 : 
+            if self.minutes == 59: 
+                self.minutes = 0
+            else: 
+                self.minutes += 1 
+            self.seconds = 0 
+        
+        
+    def __str__(self):
+        if self.minutes < 10:
+            if self.seconds < 10:
+                return f"0{self.minutes}:0{self.seconds}"
+            else:
+                return f"0{self.minutes}:{self.seconds}"
+        else:
+            if self.seconds < 10:
+                return f"{self.minutes}:0{self.seconds}"
+            else:
+                return f"{self.minutes}:{self.seconds}"
+        
+
+if __name__ == "__main__":
+    watch = Stopwatch()
+    for i in range(3600):
+        print(watch)
+        watch.tick()
+    
+ 
