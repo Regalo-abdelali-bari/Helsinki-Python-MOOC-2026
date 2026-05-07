bebbbc37f55040ee8221c064226e64357018857f
Abdelali <bariabdelali510@gmail.com>
2026-05-07 12:38:13 +0100

    Part9 - Comparing properties

---
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -0,0 +1,21 @@
+# Write your solution here:
+
+class RealProperty:
+    def __init__(self, rooms: int , square_metres: int , price_per_sqm:int):
+        self.rooms = rooms
+        self.square_metres = square_metres
+        self.price_per_sqm = price_per_sqm
+
+    def bigger(self, compared_to:  "RealProperty"):
+        return self.square_metres > compared_to.square_metres
+
+    def price_difference(self, compared_to):
+        price1 = self.square_metres *  self.price_per_sqm
+        price2 = compared_to.square_metres * compared_to.price_per_sqm 
+        return price1 - price2 if price1 > price2 else price2 - price1
+
+    def more_expensive(self, compared_to):
+        price1 = self.square_metres *  self.price_per_sqm
+        price2 = compared_to.square_metres * compared_to.price_per_sqm 
+        return price1 > price2
+        
