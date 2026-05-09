2bb2cefe4c6975383a627fb8c3b59904e5da7122
Abdelali <bariabdelali510@gmail.com>
2026-05-09 12:28:57 +0100

    Part9 - Service charge

---
+++ part09-12_service_charge/src/service_charge.py
@@ -0,0 +1,22 @@
+# WRITE YOUR SOLUTION HERE:
+class BankAccount:
+    def __init__(self,owner : str ,account_number:str , balance:float):
+        self.__owner = owner
+        self.__account_number = account_number
+        self.__balance = balance
+
+    def __service_charge(self):
+        self.__balance -= (self.__balance * 0.01)
+
+    def deposit(self,amount: float):
+        self.__balance += amount
+        self.__service_charge()
+
+    def withdraw(self,amount):
+        self.__balance -= amount
+        self.__service_charge()
+
+    @property
+    def balance(self):
+        return self.__balance
+
